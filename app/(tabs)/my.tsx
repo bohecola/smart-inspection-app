cae1b2a8bdd208956d27be045d81e1d534e1f902
bohecola <bohecola@outlook.com>
2025-11-26 16:30:34 +0800

    业务模块与路由分离

--- app/(tabs)/my.tsx
+++ app/(tabs)/my.tsx
@@ -1,65 +1,7 @@
-import { useRouter } from 'expo-router'
-import { useState } from 'react'
-import { View } from 'react-native'
-import { useDialog } from '@/components/dialog'
-import { useAppToast } from '@/components/toast'
-import { Button, ButtonSpinner, ButtonText } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
-import { Image } from '@/components/ui/image'
-import { Text } from '@/components/ui/text'
-import { useUserStore } from '@/store/user'
+import My from '@/modules/my'
 
-// eslint-disable-next-line perfectionist/sort-imports
-const defaultAvatar = require('@/assets/images/default-avatar.jpeg')
-
-export default function SettingsScreen() {
-  const router = useRouter()
-
-  const { info, logout } = useUserStore()
-
-  const { showConfirmDialog } = useDialog()
-
-  const [loading, setLoading] = useState(false)
-
-  const toast = useAppToast()
-
-  function handleLogout() {
-    showConfirmDialog({
-      description: '确定退出登录吗？',
-    })
-      .then(async () => {
-        setLoading(true)
-        await logout()
-        router.replace('/sign-in')
-        toast.show('退出成功')
-      })
-      .catch(() => {})
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-
+export default function MyPage() {
   return (
-    <View className="p-4 flex-1 bg-background-50">
-      <Card className="rounded-md">
-        <View className="flex-row items-center gap-4">
-          <Image
-            source={defaultAvatar}
-            className="w-20 h-20 rounded-full"
-            alt="默认头像"
-          />
-          <Text className="font-bold">{info?.nickName}</Text>
-        </View>
-      </Card>
-
-      <Button
-        className="mt-4"
-        disabled={loading}
-        onPress={handleLogout}
-      >
-        {loading && <ButtonSpinner color="gray" />}
-        <ButtonText>退出登录</ButtonText>
-      </Button>
-    </View>
+    <My />
   )
 }
---
+++ modules/home/index.tsx
@@ -0,0 +1,63 @@
+import type { Href } from 'expo-router'
+import { useRouter } from 'expo-router'
+import { View } from 'react-native'
+import { Card } from '@/components/ui/card'
+import { Grid, GridItem } from '@/components/ui/grid'
+import { Image } from '@/components/ui/image'
+import { Pressable } from '@/components/ui/pressable'
+import { Text } from '@/components/ui/text'
+
+// eslint-disable-next-line perfectionist/sort-imports
+const BannerImage = require('@/assets/images/banner.png')
+
+interface Menu {
+  title: string
+  imgUrl: string
+  path?: Href
+}
+
+export default function Index() {
+  const router = useRouter()
+
+  const menus: Menu[] = [
+    { title: '生产任务', imgUrl: require('@/assets/images/icons/1.png'), path: '/prod' },
+    { title: '巡检任务', imgUrl: require('@/assets/images/icons/2.png'), path: '/inspec' },
+    { title: '生产日报', imgUrl: require('@/assets/images/icons/3.png'), path: '/daily-report' },
+    { title: '缺陷', imgUrl: require('@/assets/images/icons/4.png'), path: '/bug' },
+  ]
+
+  return (
+    <View className="p-4 flex-1 bg-background-50">
+      <Image
+        source={BannerImage}
+        className="w-full h-32 rounded-xl"
+        alt="Banner"
+      />
+
+      <Card className="p-0 mt-4 flex-row flex-wrap overflow-hidden">
+        <Grid className="justify-between" _extra={{ className: 'grid-cols-12' }}>
+          {menus.map(menu => (
+            <GridItem
+              key={menu.title}
+              _extra={{ className: 'col-span-4' }}
+            >
+              <Pressable
+                className="py-6 items-center active:bg-background-100"
+                onPress={() => {
+                  router.push(menu.path)
+                }}
+              >
+                <Image
+                  source={menu.imgUrl}
+                  className="w-16 h-16"
+                  alt={menu.title}
+                />
+                <Text className="text-sm">{menu.title}</Text>
+              </Pressable>
+            </GridItem>
+          ))}
+        </Grid>
+      </Card>
+    </View>
+  )
+}

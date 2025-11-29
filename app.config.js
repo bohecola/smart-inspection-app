bf52987761c77dc639aa8ecd39f5553c31b7f090
bohecola <bohecola@outlook.com>
2025-11-29 23:05:37 +0800

    update app config

--- app.config.js
+++ app.config.js
@@ -1,5 +1,10 @@
 const { EXPO_PUBLIC_APP_NAME, APP_UNIQUE_IDENTIFIER } = process.env
 
+const cameraPermission = `允许${EXPO_PUBLIC_APP_NAME}访问相机以拍摄照片和视频`
+const microphonePermission = `允许${EXPO_PUBLIC_APP_NAME}访问麦克风以录制视频声音`
+const photosPermission = `允许${EXPO_PUBLIC_APP_NAME}访问相册以选择照片`
+const savePhotosPermission = `允许${EXPO_PUBLIC_APP_NAME}保存照片到相册`
+
 export default ({ config }) => ({
   ...config,
   name: EXPO_PUBLIC_APP_NAME,
@@ -16,9 +21,38 @@ export default ({ config }) => ({
     [
       'expo-camera',
       {
-        cameraPermission: `允许${EXPO_PUBLIC_APP_NAME}访问相机以拍摄照片和视频`,
-        microphonePermission: `允许${EXPO_PUBLIC_APP_NAME}访问麦克风以录制视频声音`,
+        cameraPermission,
+        microphonePermission,
         recordAudioAndroid: true,
+      },
+    ],
+    [
+      'expo-media-library',
+      {
+        photosPermission,
+        savePhotosPermission,
+        isAccessMediaLocationEnabled: true,
+      },
+    ],
+    [
+      'expo-image-picker',
+      {
+        photosPermission,
+        savePhotosPermission,
+        isAccessMediaLocationEnabled: true,
+      },
+    ],
+    [
+      'expo-document-picker',
+      {
+        iCloudContainerEnvironment: 'Production',
+      },
+    ],
+    [
+      'expo-video',
+      {
+        supportsBackgroundPlayback: true,
+        supportsPictureInPicture: true,
       },
     ],
   ],

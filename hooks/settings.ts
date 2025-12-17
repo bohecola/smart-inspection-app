3a6b5e5286fb5585b7f03b3b06dd10dd4db89820
bohecola <bohecola@outlook.com>
2025-12-17 14:38:00 +0800

    fix unrecognized deconstruction assignment

--- hooks/settings.ts
+++ hooks/settings.ts
@@ -1,19 +1,11 @@
 import type { GlobConfig } from 'types/config'
 
 export function useGlobSettings(): Readonly<GlobConfig> {
-  const {
-    EXPO_PUBLIC_API_URL,
-    EXPO_PUBLIC_API_URL_PREFIX,
-    EXPO_PUBLIC_APP_NAME,
-    EXPO_PUBLIC_RSA_PUBLIC_KEY,
-    EXPO_PUBLIC_CLIENT_ID,
-  } = process.env
-
   return {
-    rsaPublicKey: EXPO_PUBLIC_RSA_PUBLIC_KEY,
-    apiUrl: EXPO_PUBLIC_API_URL,
-    apiUrlPrefix: EXPO_PUBLIC_API_URL_PREFIX,
-    appName: EXPO_PUBLIC_APP_NAME ?? 'Expo App',
-    clientId: EXPO_PUBLIC_CLIENT_ID,
+    rsaPublicKey: process.env.EXPO_PUBLIC_RSA_PUBLIC_KEY,
+    apiUrl: process.env.EXPO_PUBLIC_API_URL,
+    apiUrlPrefix: process.env.EXPO_PUBLIC_API_URL_PREFIX,
+    appName: process.env.EXPO_PUBLIC_APP_NAME ?? 'Expo App',
+    clientId: process.env.EXPO_PUBLIC_CLIENT_ID,
   }
 }

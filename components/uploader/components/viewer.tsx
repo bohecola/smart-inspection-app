126def01ef052c256d3d23a6391cb084b4644c59
bohecola <bohecola@outlook.com>
2025-12-24 17:41:04 +0800

    feat: support file preview

--- components/uploader/components/viewer.tsx
+++ components/uploader/components/viewer.tsx
@@ -1,18 +1,21 @@
 import type { ImageSource } from 'react-native-image-viewing/dist/@types'
 import type { FilePreviewType, UploaderFileListItem } from '../types'
+import CryptoJS from 'crypto-js'
+import { useRouter } from 'expo-router'
 import { forwardRef, useImperativeHandle, useState } from 'react'
 import { View } from 'react-native'
 import { ImagePreview } from '@/components/image-preview'
-import { useAppToast } from '@/components/toast'
+import { encryptBase64 } from '@/utils/security'
+import { useGlobSettings } from '@/utils/settings'
 import { getFilePreviewType } from '../helper'
-
-interface ViewerProps {
-  images?: ImageSource[]
-}
 
 export interface ViewerRef {
   open: (options: OpenOptions) => void
   close: () => void
+}
+
+interface ViewerProps {
+  images?: ImageSource[]
 }
 
 interface OpenOptions {
@@ -20,43 +23,64 @@ interface OpenOptions {
 }
 
 export const Viewer = forwardRef<ViewerRef, ViewerProps>((props, ref) => {
+  // 路由
+  const router = useRouter()
+  // 预览地址
+  const { previewUrl } = useGlobSettings()
+  // 图片列表
   const { images } = props
-
-  // 提示
-  const toast = useAppToast()
-
   // 是否显示
   const [visible, setVisible] = useState(false)
-
   // 文件预览类型
   const [type, setType] = useState<FilePreviewType>(undefined)
-
-  // 打开选项
+  // 配置项
   const [openOptions, setOpenOptions] = useState<OpenOptions>({
     item: undefined,
   })
 
   // 打开
-  function open(options: OpenOptions) {
+  const open = (options: OpenOptions) => {
+    // 配置
+    setOpenOptions(options)
+    // 文件项
     const { item } = options
-
-    // 获取文件预览类型
+    // 设置文件类型
     const previewType = getFilePreviewType(item.ext)
+    setType(previewType)
 
-    if (previewType !== 'image') {
-      return toast.info(`预览功能正在开发中~`)
+    // 图片、视频预览
+    if (previewType === 'image' || previewType === 'video') {
+      return setVisible(true)
     }
 
-    setType(previewType)
-    // 设置打开选项
-    setOpenOptions(options)
-    // 设置显示
-    setVisible(true)
+    // 文档、压缩文件、其他预览
+    const uri = `${previewUrl}?url=${encodeURIComponent(encryptBase64(CryptoJS.enc.Utf8.parse(item.url)))}&t=${Date.now()}`
+    router.push({
+      pathname: `/web-screen/[uri]`,
+      params: {
+        uri,
+        title: item.fileName,
+      },
+    })
   }
 
   // 关闭
-  function close() {
-    setVisible(false)
+  const close = () => setVisible(false)
+
+  // 内容渲染
+  const renderContent = () => {
+    if (type === 'image') {
+      return (
+        <ImagePreview
+          images={images}
+          imageIndex={images.findIndex((e: any) => e.uri === openOptions.item?.url)}
+          visible={visible}
+          onRequestClose={close}
+        />
+      )
+    }
+
+    return null
   }
 
   // API
@@ -69,16 +93,7 @@ export const Viewer = forwardRef<ViewerRef, ViewerProps>((props, ref) => {
     visible
       ? (
           <View>
-            {type === 'image'
-              ? (
-                  <ImagePreview
-                    images={images}
-                    imageIndex={images.findIndex((e: any) => e.uri === openOptions.item?.url)}
-                    visible={visible}
-                    onRequestClose={close}
-                  />
-                )
-              : null}
+            {renderContent()}
           </View>
         )
       : null
